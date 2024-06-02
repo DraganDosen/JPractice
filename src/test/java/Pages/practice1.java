e2085eabcbb924f02ad6c1236a59feec0cfa99ed
DraganDosen <dragan.dosen20@gmail.com>
2024-06-02 19:23:30 +0200

    commit

--- src/test/java/Pages/practice1.java
+++ src/test/java/Pages/practice1.java
@@ -1,41 +1,36 @@
 package Pages;
 
 public class practice1 {
-	
-	public static int find(int param){
-		int a= param;
-		int sum =0;
+
+	public static int find(int param) {
+		int a = param;
+		int sum = 0;
 		int b;
-		int f=0;
+		int f = 0;
 		System.out.println(a);
-		
-		
-	   
-	  System.out.println(a);
-		//int sum = b+c;
+
+		System.out.println(a);
+		// int sum = b+c;
 		while (a != 0) {
-		  b = a%10;
-           a = a/10;
-           System.out.println("a is: "+a);
-           System.out.println("b is: "+b);
-           sum = sum +b;
-           System.out.println("sum is: "+sum);
-           
+			b = a % 10;
+			a = a / 10;
+			System.out.println("a is: " + a);
+			System.out.println("b is: " + b);
+			sum = sum + b;
+			System.out.println("sum is: " + sum);
+
 		}
 		return (sum);
-		
-		
+
 	};
 
 	public static void main(String[] args) {
-		
-		//System.out.println("Hello World");
-		int a =293;
+
+		// System.out.println("Hello World");
+		int a = 293;
 		find(a);
 		System.out.println(find(a));
 		// TODO Auto-generated method stub
 	}
-    
-	}
 
-
+}
